5fd2434a5ecaf9bc2c8f0af77aa640678d43244c
lancej1022 <lancej1022@gmail.com>
2020-07-11 23:53:13 -0700

    Changed back to class syntax, will review to see if Hooks are plausible

--- src/Config.js
+++ src/Config.js
@@ -19,149 +19,47 @@ function createEditorInterface(selectedCt) {
   return { [selectedCt.sys.id]: { controls: [{ fieldId: targetField.id }] } };
 }
 
-/**
- * destucture app, space off props
- * @param {*} param0
- */
-const Config = (props) => {
-  const { app, space } = props.sdk;
-  const [selectedContentTypeId, setSelectedContentTypeId] = useState('');
-  const [contentTypes, setContentTypes] = useState([]);
+// /**
+//  * destucture app, space off props
+//  * @param {*} param0
+//  */
+// const Config = (props) => {
+//   const { app, space } = props.sdk;
+//   const [selectedContentTypeId, setSelectedContentTypeId] = useState('');
+//   const [contentTypes, setContentTypes] = useState([]);
 
-  useEffect(() => {
-    console.log('firing');
-    async function configApp() {
-      app.onConfigure(onConfigure);
+//   useEffect(() => {
+//     console.log('firing');
+//     async function configApp() {
+//       app.onConfigure(onConfigure);
 
-      const [ctsRes, parameters] = await Promise.all([
-        space.getContentTypes(),
-        app.getParameters(),
-      ]);
+//       const [ctsRes, parameters] = await Promise.all([
+//         space.getContentTypes(),
+//         app.getParameters(),
+//       ]);
 
-      const formattedContentTypes = ctsRes ? ctsRes.items : [];
+//       const formattedContentTypes = ctsRes ? ctsRes.items : [];
 
-      // update state
-      setContentTypes(formattedContentTypes);
-      if (parameters) {
-        setSelectedContentTypeId();
-      } else {
-        setSelectedContentTypeId(get(formattedContentTypes, [0, 'sys', 'id'], ''));
-      }
+//       // update state
+//       setContentTypes(formattedContentTypes);
+//       if (parameters) {
+//         setSelectedContentTypeId();
+//       } else {
+//         setSelectedContentTypeId(get(formattedContentTypes, [0, 'sys', 'id'], ''));
+//       }
 
-      () => app.setReady();
-    }
-    configApp();
-  });
+//       () => app.setReady();
+//     }
+//     configApp();
+//   });
 
-  async function onConfigure() {
-    const selectedContentType = contentTypes.find(
-      (ct) => ct.sys.id === selectedContentTypeId
-    );
-
-    if (!selectedContentType) {
-      props.sdk.notifier.error('You do not have any content types to select!');
-      return false;
-    }
-
-    return {
-      parameters: {
-        selectedContentTypeId,
-      },
-      targetState: {
-        EditorInterface: createEditorInterface(selectedContentType),
-      },
-    };
-  }
-
-  const pickCt = (id) => {
-    setSelectedContentTypeId(id);
-  };
-
-  let body = (
-    <Typography>
-      <Heading>You do not have any content types!</Heading>
-      <Paragraph>First create a content type with a JSON field to continue.</Paragraph>
-    </Typography>
-  );
-
-  if (contentTypes.length) {
-    body = (
-      <Select value={selectedContentTypeId} onChange={(e) => pickCt(e.target.value)}>
-        {contentTypes.map((ct) => (
-          <Option key={ct.sys.id} value={ct.sys.id}>
-            {ct.name}
-          </Option>
-        ))}
-      </Select>
-    );
-  }
-
-  return (
-    <div className="app">
-      <div className="background" />
-      <div className="body">
-        <div className="config">
-          <Typography>
-            <Heading>Unsplash</Heading>
-            <Paragraph>
-              Ayyyy lmao
-              <code> JSON</code> field.
-            </Paragraph>
-          </Typography>
-          {body}
-        </div>
-      </div>
-      <div className="logo" />
-    </div>
-  );
-};
-
-Config.propTypes = {
-  sdk: PropTypes.object,
-};
-
-// export default class Config extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       selectedContentTypeId: '',
-//       contentTypes: [],
-//     };
-//   }
-
-//   async componentDidMount() {
-//     const { app, space } = this.props.sdk;
-//     app.onConfigure(this.onConfigure);
-
-//     const [ctsRes, parameters] = await Promise.all([
-//       space.getContentTypes(),
-//       app.getParameters(),
-//     ]);
-
-//     const formattedContentTypes = ctsRes ? ctsRes.items : [];
-
-//     // eslint-disable-next-line react/no-did-mount-set-state
-//     this.setState(
-//       {
-//         contentTypes: formattedContentTypes,
-//         selectedContentTypeId: parameters
-//           ? parameters.selectedContentTypeId
-//           : get(formattedContentTypes, [0, 'sys', 'id'], ''),
-//       },
-//       () => app.setReady()
-//     );
-//   }
-
-//   onConfigure = async () => {
-//     const { selectedContentTypeId, contentTypes } = this.state;
-
+//   async function onConfigure() {
 //     const selectedContentType = contentTypes.find(
 //       (ct) => ct.sys.id === selectedContentTypeId
 //     );
 
 //     if (!selectedContentType) {
-//       this.props.sdk.notifier.error('You do not have any content types to select!');
+//       props.sdk.notifier.error('You do not have any content types to select!');
 //       return false;
 //     }
 
@@ -173,54 +71,155 @@ Config.propTypes = {
 //         EditorInterface: createEditorInterface(selectedContentType),
 //       },
 //     };
+//   }
+
+//   const pickCt = (id) => {
+//     setSelectedContentTypeId(id);
 //   };
 
-//   pickCt = (id) => {
-//     this.setState({ selectedContentTypeId: id });
-//   };
+//   let body = (
+//     <Typography>
+//       <Heading>You do not have any content types!</Heading>
+//       <Paragraph>First create a content type with a JSON field to continue.</Paragraph>
+//     </Typography>
+//   );
 
-//   render() {
-//     let body = (
-//       <Typography>
-//         <Heading>You do not have any content types!</Heading>
-//         <Paragraph>First create a content type with a JSON field to continue.</Paragraph>
-//       </Typography>
-//     );
-
-//     if (this.state.contentTypes.length) {
-//       body = (
-//         <Select
-//           value={this.state.selectedContentTypeId}
-//           onChange={(e) => this.pickCt(e.target.value)}
-//         >
-//           {this.state.contentTypes.map((ct) => (
-//             <Option key={ct.sys.id} value={ct.sys.id}>
-//               {ct.name}
-//             </Option>
-//           ))}
-//         </Select>
-//       );
-//     }
-
-//     return (
-//       <div className="app">
-//         <div className="background" />
-//         <div className="body">
-//           <div className="config">
-//             <Typography>
-//               <Heading>Unsplash</Heading>
-//               <Paragraph>
-//                 Ayyyy lmao
-//                 <code> JSON</code> field.
-//               </Paragraph>
-//             </Typography>
-//             {body}
-//           </div>
-//         </div>
-//         <div className="logo" />
-//       </div>
+//   if (contentTypes.length) {
+//     body = (
+//       <Select value={selectedContentTypeId} onChange={(e) => pickCt(e.target.value)}>
+//         {contentTypes.map((ct) => (
+//           <Option key={ct.sys.id} value={ct.sys.id}>
+//             {ct.name}
+//           </Option>
+//         ))}
+//       </Select>
 //     );
 //   }
-// }
 
-export default Config;
+//   return (
+//     <div className="app">
+//       <div className="background" />
+//       <div className="body">
+//         <div className="config">
+//           <Typography>
+//             <Heading>Unsplash</Heading>
+//             <Paragraph>
+//               Ayyyy lmao
+//               <code> JSON</code> field.
+//             </Paragraph>
+//           </Typography>
+//           {body}
+//         </div>
+//       </div>
+//       <div className="logo" />
+//     </div>
+//   );
+// };
+
+// Config.propTypes = {
+//   sdk: PropTypes.object,
+// };
+
+// export default Config;
+export default class Config extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedContentTypeId: '',
+      contentTypes: [],
+    };
+  }
+
+  async componentDidMount() {
+    const { app, space } = this.props.sdk;
+    app.onConfigure(this.onConfigure);
+
+    const [ctsRes, parameters] = await Promise.all([
+      space.getContentTypes(),
+      app.getParameters(),
+    ]);
+
+    const formattedContentTypes = ctsRes ? ctsRes.items : [];
+
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState(
+      {
+        contentTypes: formattedContentTypes,
+        selectedContentTypeId: parameters
+          ? parameters.selectedContentTypeId
+          : get(formattedContentTypes, [0, 'sys', 'id'], ''),
+      },
+      () => app.setReady()
+    );
+  }
+
+  onConfigure = async () => {
+    const { selectedContentTypeId, contentTypes } = this.state;
+
+    const selectedContentType = contentTypes.find(
+      (ct) => ct.sys.id === selectedContentTypeId
+    );
+
+    if (!selectedContentType) {
+      this.props.sdk.notifier.error('You do not have any content types to select!');
+      return false;
+    }
+
+    return {
+      parameters: {
+        selectedContentTypeId,
+      },
+      targetState: {
+        EditorInterface: createEditorInterface(selectedContentType),
+      },
+    };
+  };
+
+  pickCt = (id) => {
+    this.setState({ selectedContentTypeId: id });
+  };
+
+  render() {
+    let body = (
+      <Typography>
+        <Heading>You do not have any content types!</Heading>
+        <Paragraph>First create a content type with a JSON field to continue.</Paragraph>
+      </Typography>
+    );
+
+    if (this.state.contentTypes.length) {
+      body = (
+        <Select
+          value={this.state.selectedContentTypeId}
+          onChange={(e) => this.pickCt(e.target.value)}
+        >
+          {this.state.contentTypes.map((ct) => (
+            <Option key={ct.sys.id} value={ct.sys.id}>
+              {ct.name}
+            </Option>
+          ))}
+        </Select>
+      );
+    }
+
+    return (
+      <div className="app">
+        <div className="background" />
+        <div className="body">
+          <div className="config">
+            <Typography>
+              <Heading>Unsplash</Heading>
+              <Paragraph>
+                Ayyyy lmao
+                <code> JSON</code> field.
+              </Paragraph>
+            </Typography>
+            {body}
+          </div>
+        </div>
+        <div className="logo" />
+      </div>
+    );
+  }
+}
