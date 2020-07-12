5ad22bacb635aaa96903e7eae9d5cc8f7e6ece3d
lancej1022 <lancej1022@gmail.com>
2020-07-11 22:19:02 -0700

    Config boilerplate

--- src/index.js
+++ src/index.js
@@ -1,62 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
-import { TextInput } from '@contentful/forma-36-react-components';
-import { init, AppExtensionSDK, locations } from 'contentful-ui-extensions-sdk';
+import React from 'react';
+import { render } from 'react-dom';
+import { init, locations } from 'contentful-ui-extensions-sdk';
+import Config from './Config';
+import FieldEditor from './FieldEditor';
+import Dialog from './Dialog';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.scss';
 
-export const App = ({ sdk }) => {
-  const [value, setValue] = useState(sdk.field.getValue() || '');
-
-  const onExternalChange = (value) => {
-    setValue(value);
-  };
-
-  const onChange = (e) => {
-    const value = e.currentTarget.value;
-    setValue(value);
-    if (value) {
-      sdk.field.setValue(value);
-    } else {
-      sdk.field.removeValue();
-    }
-  };
-
-  useEffect(() => {
-    sdk.window.startAutoResizer();
-  }, [sdk.window]);
-
-  useEffect(() => {
-    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-    const detatchValueChangeHandler = sdk.field.onValueChanged(onExternalChange);
-    return detatchValueChangeHandler;
-  });
-
-  return (
-    <TextInput
-      width="large"
-      type="text"
-      id="my-field"
-      testId="my-field"
-      value={value}
-      onChange={onChange}
-    />
-  );
-};
-
-App.propTypes = {
-  sdk: PropTypes.object.isRequired,
-};
+function renderAtRoot(component) {
+  return render(component, document.getElementById('root'));
+}
 
 init((sdk) => {
-  ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
+  if (sdk.location.is(locations.LOCATION_APP_CONFIG)) {
+    renderAtRoot(<Config sdk={sdk} />);
+  } else if (sdk.location.is(locations.LOCATION_ENTRY_FIELD)) {
+    renderAtRoot(<FieldEditor sdk={sdk} />);
+  } else if (sdk.location.is(locations.LOCATION_DIALOG)) {
+    renderAtRoot(<Dialog sdk={sdk} />);
+  }
 });
-
-/**
- * By default, iframe of the extension is fully reloaded on every save of a source file.
- * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
- */
-// if (module.hot) {
-//   module.hot.accept();
-// }
