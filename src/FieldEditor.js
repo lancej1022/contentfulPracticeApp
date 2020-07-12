f953b8a3a32dc2c571d8b61b1ddedfd03d013922
lancej1022 <lancej1022@gmail.com>
2020-07-11 22:34:55 -0700

    Unsplash API connection and FieldEditor component

---
+++ src/FieldEditor.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Button } from '@contentful/forma-36-react-components';
+
+export default class FieldEditor {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: props.sdk.field.getValue() || null,
+    };
+  }
+
+  componentDidMount() {
+    this.props.sdk.window.startAutoResizer();
+
+    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(
+      this.onExternalChange
+    );
+  }
+
+  componentWillUnmount() {
+    if (this.detachExternalChangeHandler) {
+      this.detachExternalChangeHandler();
+    }
+  }
+
+  onExternalChange = (value) => {
+    this.setState({ value });
+  };
+
+  openSearch = async () => {
+    const selectedPhoto = await this.props.sdk.dialogs.openCurrentApp({
+      title: 'Select a photo',
+      minHeight: 768,
+      width: 'large',
+    });
+
+    if (selectedPhoto) {
+      this.props.sdk.field.setValue(selectedPhoto);
+    }
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <div>
+        <div>{!!value && <img src={value.urls.thumb} alt={value.alt_description} />}</div>
+        <Button onClick={this.openSearch}>Open Search</Button>
+      </div>
+    );
+  }
+}
